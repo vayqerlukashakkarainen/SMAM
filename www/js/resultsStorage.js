f4b4823384609d897e8c3b5055424a274d3dbf5b
Lukas Hakkarainen <lukas@vayqer.se>
2022-03-28 14:57:01 +0200

    start

---
+++ www/js/resultsStorage.js
@@ -0,0 +1,12 @@
+var resultStorageContainer = {
+	savedResults: null,
+	historyResults: null,
+	ratedResults: null,
+
+	maxHistoryResults: 40,
+
+	Setup: function () {},
+	InsertResultInHistory: function (result) {},
+	InsertResultInSaved: function (results) {},
+	InsertResultInRated: function (results) {},
+};
